749b76502c344c0828b21e9dd5afda96ecdbf60b
butterw <butterw@gmail.com>
2020-10-15 12:17:54 +0200

    Update and rename Dots-bi.hlsl to grey-Dots.hlsl

---
+++ Effects/grey-Dots.hlsl
@@ -0,0 +1,30 @@
+#define Scale 0.4 //[0.3, 0.6] default: 0.4. Higher for smaller dots
+
+/*
+gr-Dots.hlsl (mpc-hc dx9 pixel shader)
+ported by butterw from https://github.com/pixijs/pixi-filters/blob/master/filters/dot/src/dot.frag (MIT License)
+
+Output is grayscale, mostly black and white
+Can be used Post-resize, can use 2instances.
+(1 texture, 31 arithmetic)
+*/
+sampler s0: register(s0);
+float2  p0: register(c0); //W, H
+#define CoefLuma float4(0.2126, 0.7152, 0.0722, 0) // BT.709 & sRBG luma coef (Monitors, HDTV)
+
+float pattern(float2 tex){
+    #define Angle 5.0 //0: flat
+    float s = sin(Angle), c = cos(Angle);
+    float2 coord = tex * p0;
+	float2 pt = Scale *float2(c*coord.x - s*coord.y, s*coord.x + c*coord.y);
+    return 4 *sin(pt.x) *sin(pt.y);
+}
+
+float4 main(float2 tex: TEXCOORD0): COLOR {
+    // return pattern(tex); //Show dot pattern
+    float4 color = tex2D(s0, tex);
+    float  luma  = dot(color, CoefLuma);
+	luma = 10*luma-5 + pattern(tex);
+    // return clamp(luma, 16/255., 245/255.); //clamp gray output
+	return luma;
+}
