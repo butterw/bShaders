19944f600b01e362dedd05247c6b56a643e4bfe5
butterw <butterw@gmail.com>
2020-10-24 20:13:25 +0200

    updated mpv CrossHatch.hook.glsl

--- Effects/CrossHatch.hook.glsl
+++ Effects/CrossHatch.hook.glsl
@@ -2,29 +2,35 @@
 //!BIND HOOKED
 //!DESC CrossHatch
 
-/* pixi.CrossHatch (style Effect for video content)
+/* CrossHatch (style Effect for video content)
 A different effect is produced based on HOOK point: LUMA (colors) or OUTPUT/MAIN (Red + 2 grays + White) 
 
 adapted to mpv .hook by butterw 
 from https://github.com/pixijs/pixi-filters/blob/master/filters/cross-hatch/src/crosshatch.frag (MIT License)
 */
 
+#define Hatch_mode 2 //default: 2 [0, 1, 2, 3] 
 #define White vec4(245, 245, 245, 255)/255.
 
 vec4 hook(){
+#define _hatch 1.25*pow(2, Hatch_mode) 
     vec4 color = White; //Background Color
     float lum = length(HOOKED_texOff(0).rgb); //Gray
-
-    if ((lum < 1.) && mod(gl_FragCoord.x +gl_FragCoord.y, 10.) == 0.) // */*
+	
+	float coords_sum  = gl_FragCoord.x + gl_FragCoord.y;
+	float coords_diff = gl_FragCoord.x - gl_FragCoord.y;
+	
+	
+    if ((lum < 1.) && mod(coords_sum, _hatch) == 0.) // */*
 		color = vec4(0.25);
 
-    if ((lum < 0.75) && mod(gl_FragCoord.x -gl_FragCoord.y, 10.) == 0.) // *\*
+    if ((lum < 0.75) && mod(coords_diff, _hatch) == 0.) // *\*
         color = vec4(1., 0, 0, 1); //Red
 
-    if ((lum < 0.50) && mod(gl_FragCoord.x + gl_FragCoord.y -5., 10.) == 0.) // */offset*
+    if ((lum < 0.50) && mod(coords_sum -5, _hatch) == 0.) // */offset*
         color = vec4(0.5);
 
-    if ((lum < 0.3) && mod(gl_FragCoord.x - gl_FragCoord.y -5., 10.) == 0.0)// *\offset* 
+    if ((lum < 0.3) && mod(coords_diff -5, _hatch) == 0.0)// *\offset* 
         color = vec4(0.5);
 return color;
 }
