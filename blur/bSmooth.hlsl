7bfc6d5d9bea38d9d7e9e5a7fb6efc2eefc3cb96
butterw <butterw@gmail.com>
2020-11-16 18:00:59 +0100

    Update bSmooth.hlsl

--- blur/bSmooth.hlsl
+++ blur/bSmooth.hlsl
@@ -1,6 +1,6 @@
 #define Mode 1 
 /* Mode [0, 1] default:0 full (9 texture, 15 arithmetic), 
-// 1: hw, uses hw linear sampling (4 texture, 7 arithmetic)
+// 1: uses hw linear sampling (4 texture, 7 arithmetic)
 
 Gaussian Smoothing Filter
 by butterw, License: GPL v3
@@ -15,7 +15,7 @@ for maximum effect use pre-upscaling.
 
 sampler s0: register(s0);
 float2  p1: register(c1); 
-#define px p1.x // pixel "width"
+#define px p1.x // pixel width
 #define py p1.y
 
 float4 main(float2 tex: TEXCOORD0): COLOR {
@@ -24,13 +24,13 @@ float4 main(float2 tex: TEXCOORD0): COLOR {
 
 
 #if Mode==1 //hw.gSmooth3(4 texture, 7 arithmetic)
-    #define po 0.499 //default: 0.5, 0.499: doesn't blur if hw linear sampling isn't supported. 
+    #define po 0.499 //default: 0.5, 0.499: no blur if hw linear sampling isn't supported. 
     blur = tex2D(s0, tex + p1*float2(-po, -po));
     blur+= tex2D(s0, tex + p1*float2( po, -po));
     blur+= tex2D(s0, tex + p1*float2(-po, po));
     blur+= tex2D(s0, tex + p1*float2( po, po));
     blur*= 0.25;
-    return 10*abs(orig-blur); //debug with po 0.499: if hw linear sampling isn't supported: 100% black screen 
+    return 10*abs(orig-blur); //debug output with po 0.499, if hw linear sampling isn't supported: 100% black screen 
     return blur;
 #endif
 
