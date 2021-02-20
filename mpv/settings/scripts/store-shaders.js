bb89bc548aaa23e3ce81e4ea6c4e4d6a11893477
butterw <butterw@gmail.com>
2021-02-20 18:22:29 +0100

    Update store-shaders.js

--- mpv/settings/scripts/store-shaders.js
+++ mpv/settings/scripts/store-shaders.js
@@ -1,19 +1,20 @@
 'use strict';
 
-// store/restore shaders script for mpv by butterw
+// store/restore shaders script for mpv by butterw (v0.1)
 // put in .\portable_config\scripts mpv subfolder. 
-// input.conf: F9 script-message store-shaders
+// input.conf: F10 script-message store-shaders
 // https://mpv.io/manual/stable/#javascript
 
 var is_first_run = true 
 var shaders = ""
 function store_shaders() { //store current shader config on first run, this config will be restored by subsequent calls 
-	if (is_first_run) { 
-	print("store-shaders", is_first_run)
-	shaders = mp.get_property("glsl-shaders")	
+	if (is_first_run ) { 
+		// print("store-shaders", is_first_run)
+		var s = mp.get_property("glsl-shaders")
+		if (s) { shaders = s; is_first_run = false;}	
 	} else { mp.set_property("glsl-shaders", shaders.replace(/,/g, ";"));} //js global string replacement !
 	is_first_run = false;
 }
 mp.register_script_message("store-shaders", store_shaders); 
-mp.add_key_binding("F9", "store-shaders", store_shaders); //hotkey will be overriden by input.conf if a binding for the same key is defined or pre-defined.
+mp.add_key_binding("F10", "store-shaders", store_shaders); //hotkey will be overriden by input.conf if a binding for the same key is defined or pre-defined.
 store_shaders() //optional: auto-store initial shader config 
