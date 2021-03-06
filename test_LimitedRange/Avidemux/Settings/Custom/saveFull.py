1b0ab8013d336da572cff36fe1b3c0ef37ed46be
butterw <butterw@gmail.com>
2021-03-06 11:12:59 +0100

    Avidemux plugin: saveFull.py

---
+++ test_LimitedRange/Avidemux/Settings/Custom/saveFull.py
@@ -0,0 +1,38 @@
+# Save the full video not just the selection without resetting markers
+# also displays some extra info in the title of the save dialog (duration in minutes, nbSegments / nbVideos)
+# this script can be installed to settings/custom GUI menu
+
+header_str = "saveFull" # by butterw, tested in Avidemux2.7.7 
+
+adm=Avidemux(); ed=Editor(); gui=Gui() 
+sec = 1000*1000
+
+def fileWriteSelect(title="Please Select output file", ext=None):
+	# by default uses the GUI output extension in the WriteSelect Dialog 	
+	#ex: fileWriteSelect(); fileWriteSelect("Title"); fileWriteSelect(ext="mp4"); fileWriteSelect("Title", "mp4") 
+	if ext is None: ext=adm.getOutputExtension()
+	return gui.fileWriteSelectEx(title, ext) #Req Avidemux>=2.7.7
+
+nSegm = ed.nbSegments()
+if not nSegm:
+	gui.displayError(header_str, "No video loaded !")
+	return
+A = adm.markerA
+B = adm.markerB
+end = ed.getVideoDuration()
+title = str(round(end*1/sec *1/60)) +"m "
+title+= str(nSegm) +"/"+ str(ed.nbVideos()) 
+title+= " Select file to save(FULL)"
+fpath = fileWriteSelect(title)
+if fpath is None: gui.displayError(header_str, "No Output file selected !"); return
+adm.markerA = 0
+adm.markerB = end
+adm.save(fpath)
+adm.markerA = A
+adm.markerB = B
+
+
+
+
+
+	
