d55341fc678bdd112a32d3098e2819fc29af94c8
butterw <butterw@gmail.com>
2021-03-06 16:15:03 +0100

    Update saveFull.py

--- test_LimitedRange/Avidemux/Settings/Custom/saveFull.py
+++ test_LimitedRange/Avidemux/Settings/Custom/saveFull.py
@@ -1,8 +1,9 @@
 # Save the full video not just the selection without resetting markers
-# also displays some extra info in the title of the save dialog (duration in minutes, nbSegments / nbVideos)
-# this script can be installed to settings/custom GUI menu
+# also displays some extra info in the title of the save dialog
+# duration in minutes, nbSegments / nbVideos, the number of audio tracks if it is not one, ex: 1m 2/1 0a
+# https://github.com/butterw/bShaders/blob/master/test_LimitedRange  
 
-header_str = "saveFull" # by butterw, tested in Avidemux2.7.7 
+header_str = "saveFull" # v0.1 #tested in Avidemux2.8
 
 adm=Avidemux(); ed=Editor(); gui=Gui() 
 sec = 1000*1000
@@ -17,14 +18,19 @@ nSegm = ed.nbSegments()
 if not nSegm:
 	gui.displayError(header_str, "No video loaded !")
 	return
-A = adm.markerA
-B = adm.markerB
+nAudio = adm.audioTracksCount()
+audio_str=""
+if nAudio==0 or nAudio>1: 
+	audio_str= " " + str(nAudio) +"a"
 end = ed.getVideoDuration()
 title = str(round(end*1/sec *1/60)) +"m "
-title+= str(nSegm) +"/"+ str(ed.nbVideos()) 
-title+= " Select file to save(FULL)"
+title+= str(nSegm) +"/"+ str(ed.nbVideos())
+title+= audio_str
+title+= ": Select file to save(FULL)"
 fpath = fileWriteSelect(title)
 if fpath is None: gui.displayError(header_str, "No Output file selected !"); return
+A = adm.markerA
+B = adm.markerB
 adm.markerA = 0
 adm.markerB = end
 adm.save(fpath)
