7e593fac94fcfda011f9c89e473b84c85b1c7cb6
butterw <butterw@gmail.com>
2021-03-06 09:59:35 +0100

    Avidemux: black_A-B.py

--- test_LimitedRange/Avidemux/Settings/Custom/black_A-B.py
+++ test_LimitedRange/Avidemux/Settings/Custom/black_A-B.py
@@ -1,1 +1,7 @@
+# adds filter (black) based on marker A and B positions.
 
+adm=Avidemux(); ed=Editor(); gui=Gui() 
+if not ed.nbSegments():
+	gui.displayError("black_A[B", "No video loaded !")
+	return
+adm.addVideoFilter("black", "startBlack="+str(adm.markerA/1000), "endBlack="+str(adm.markerB/1000))
