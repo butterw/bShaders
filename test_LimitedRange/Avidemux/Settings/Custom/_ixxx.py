886c3e87f89d3c94160b8359e86676b9f753536a
butterw <butterw@gmail.com>
2021-03-07 22:14:12 +0100

    v0.13

--- test_LimitedRange/Avidemux/Settings/Custom/_ixxx.py
+++ test_LimitedRange/Avidemux/Settings/Custom/_ixxx.py
@@ -1,17 +1,16 @@
 # Configurable seek (Forward) 
 # requires Avidemux>=v2.7.7
-# this script can be installed to GUI menu (copy to settings/custom) 
+# install this script to the Custom menu (copy to settings/custom) 
 # https://github.com/butterw/bShaders/blob/master/test_LimitedRange
-#_ixxx.py v0.12
+#_ixxx.py v0.13
 
 seek_s=120
-## 3) keyframe-seek, using a approximate time (seek_s) in seconds 
+## 3) keyframe-seek of about (seek_s) in seconds 
 
 adm=Avidemux(); ed=Editor()
 sec = 1000*1000
 
 if not ed.nbSegments(): return
-
 c = ed.getCurrentPts()
 adm.setCurrentPts(c +seek_s*sec)
 if c<20*sec: adm.seekKeyFrame(1)
