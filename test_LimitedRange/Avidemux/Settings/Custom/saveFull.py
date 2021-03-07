24ef98fffc00058d192545c02891dc2e77d0cf00
butterw <butterw@gmail.com>
2021-03-07 12:23:12 +0100

    avidemux: saveFull.py v0.2

--- test_LimitedRange/Avidemux/Settings/Custom/saveFull.py
+++ test_LimitedRange/Avidemux/Settings/Custom/saveFull.py
@@ -1,34 +1,46 @@
 # Save the full video not just the selection without resetting markers
-# also displays some extra info in the title of the save dialog
-# duration in minutes, nbSegments / nbVideos, the number of audio tracks if it is not one, ex: 1m 2/1 0a
-# https://github.com/butterw/bShaders/blob/master/test_LimitedRange  
+# also displays some extra info in the title of the save dialog (ex: 1min20 2/1 0a)
+# duration in minutes+seconds, if more than one: nbSegments / nbVideos, the number of audio tracks 
+# this script can be installed to settings/custom GUI menu
+# https://github.com/butterw/bShaders/blob/master/test_LimitedRange
 
-header_str = "saveFull" # v0.1 #tested in Avidemux2.7.8
-
+header_str = "saveFull" ##v0.2
 adm=Avidemux(); ed=Editor(); gui=Gui() 
-sec = 1000*1000
 
 def fileWriteSelect(title="Please Select output file", ext=None):
 	# by default uses the GUI output extension in the WriteSelect Dialog 	
 	#ex: fileWriteSelect(); fileWriteSelect("Title"); fileWriteSelect(ext="mp4"); fileWriteSelect("Title", "mp4") 
 	if ext is None: ext=adm.getOutputExtension()
-	return gui.fileWriteSelectEx(title, ext) #Req Avidemux>=2.7.7
+	return gui.fileWriteSelectEx(title, ext) #Req Avidemux>=2.7.7	
+
+def toMinutesSeconds(pts):
+	# ex result: "2min43"
+	sec = 1000*1000
+	seconds = pts*1/sec
+	minutes = int(seconds*1/60)
+	seconds = round(seconds - minutes*60)
+	str0=""; if seconds<10: str0="0"
+	return str(minutes) +"min" +str0+str(seconds)
 
 nSegm = ed.nbSegments()
 if not nSegm:
 	gui.displayError(header_str, "No video loaded !")
 	return
+
 nAudio = adm.audioTracksCount()
 audio_str=""
 if nAudio==0 or nAudio>1: 
 	audio_str= " " + str(nAudio) +"a"
 end = ed.getVideoDuration()
-title = str(round(end*1/sec *1/60)) +"m "
-title+= str(nSegm) +"/"+ str(ed.nbVideos())
+title = toMinutesSeconds(end) + " " 
+segm_str = str(nSegm) +"/"+ str(ed.nbVideos())
+if segm_str != "1/1": title+= segm_str 
 title+= audio_str
-title+= ": Select file to save(FULL)"
+title+= ": Select file to save(Full)"
+ 
 fpath = fileWriteSelect(title)
 if fpath is None: gui.displayError(header_str, "No Output file selected !"); return
+# Full Save without losing Markers:
 A = adm.markerA
 B = adm.markerB
 adm.markerA = 0
@@ -36,9 +48,3 @@ adm.markerB = end
 adm.save(fpath)
 adm.markerA = A
 adm.markerB = B
-
-
-
-
-
-	
