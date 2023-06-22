4f9af1d2fa533ee09e7b91fa3cbd7244b78bb5d2
butterw <butterw@gmail.com>
2023-06-22 09:30:44 +0200

    Add files via upload

---
+++ A-pack/Shaders/bw_luma.hlsl
@@ -0,0 +1,18 @@
+// $MinimumShaderProfile: ps_2_0
+
+/* --- bw_luma (dx9) --- */
+/* released by butterw under GPLv3
+
+Displays the grayscale luminance (luma) image derived from rgb.
+Same shader as the Grayscale shader included with mpc-be (different from the one in mpc-hc).
+
+
+*/
+sampler s0: register(s0);
+#define CoefLuma float4(0.2126, 0.7152, 0.0722, 0) //sRGB, HDTV
+
+float4 main(float2 tex: TEXCOORD0): COLOR {
+	float4 c0 = tex2D(s0, tex);
+
+	return dot(c0, CoefLuma);
+}
