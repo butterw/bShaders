dc83b34ee8f997ab5617d891592a5d57f321613c
butterw <butterw@gmail.com>
2023-06-22 13:43:58 +0200

    Add files via upload

---
+++ A-pack/Shaders11/bDim-35.hlsl
@@ -0,0 +1,25 @@
+// $MinimumShaderProfile: ps_4_0
+#define Exposure -0.35
+
+/* --- bExposure (dx11) --- */
+/* v1.01 (06/2023) released by butterw under GPLv3
+(1 texture, 1 arithmetic) 
+
+Dims or lights-up the video.
+
+c0 = c0 *(1.0+Exposure) = c0 + c0*Exposure, with c0: pixel.rgb 
+- parameter Exposure, [-1, ..], 0: no change, -1: All black. 
+negative: dims, ex: -0.35
+positive: lights-up
+
+*/
+
+
+Texture2D tex : register(t0);
+SamplerState samp : register(s0);
+
+float4 main(float4 pos : SV_POSITION, float2 coord : TEXCOORD) : SV_Target {
+	float4 c0 = tex.Sample(samp, coord);
+
+	return c0 *(1.0 + Exposure);
+}
