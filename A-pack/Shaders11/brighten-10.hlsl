4ef28c160b092534fb1c668d2219b7c57ea9c60d
butterw <butterw@gmail.com>
2023-06-22 13:33:16 +0200

    Add files via upload

---
+++ A-pack/Shaders11/brighten-10.hlsl
@@ -0,0 +1,25 @@
+// $MinimumShaderProfile: ps_4_0
+#define Exposure -0.10
+
+/* --- bExposure (dx11) --- */
+/* v1.01 (06/2023) released by butterw under GPLv3
+(1 texture, 1 arithmetic)
+
+Exposure is an alternative way to increase/decrease brightness.
+
+c0 = c0 *(1.0+Exposure) = c0 + c0*Exposure, with c0: pixel.rgb 
+- parameter Exposure, [-1, 1], 0: no change 
+negative decreases brightness, ex: -0.10 
+
+*/
+
+*/
+
+Texture2D tex : register(t0);
+SamplerState samp : register(s0);
+
+float4 main(float4 pos : SV_POSITION, float2 coord : TEXCOORD) : SV_Target {
+	float4 c0 = tex.Sample(samp, coord);
+
+	return c0 *(1.0 + Exposure);
+}
