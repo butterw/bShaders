b8411c5c57a815f9fd2e8a3e7708ac2a55423592
butterw <butterw@gmail.com>
2023-06-22 10:09:47 +0200

    Update bContrast.hlsl
    v1.01

--- A-pack/Shaders/bContrast.hlsl
+++ A-pack/Shaders/bContrast.hlsl
@@ -1,26 +1,26 @@
 // $MinimumShaderProfile: ps_2_0
-#define a 10
-#define b 245
+#define bk 10
+#define wh 245
 
 /* --- bContrast, aka 16-235mod to 0-255 (dx9) --- */
-/* v1.0 (06/2023) released by butterw under GPLv3
+/* v1.01 (06/2023) released by butterw under GPLv3
 (1 texture, 1 arithmetic)
 
-modified version of the MPC-HC shader "16-235 to 0-255.hlsl". 
-allows custom values instead of just (16, 235) for (a, b)
+modified version of the MPC-HC shader "16-235 to 0-255.hlsl".
+allows custom values instead of just (16, 235) for the rgb black and white points.
 
+Default parameters (bk: 10, wh: 245) for the rgb black and white points.
 pixel.rgb = (pixel.rgb -const_1) *const_2 = pixel.rgb*const_2 -const_1*const_2
-Default parameters (a: 10, b: 245)
-Expands the rgb histogram, increasing the contrast. 
-Can be useful to lift the haze on lackluster internet videos.  
+Expands the rgb histogram, increasing the contrast.
+Can be useful to lift the haze on lackluster internet videos.
 
 */
 sampler s0: register(s0);
-#define const_1 ( a/255.)
-#define const_2 (255.0/(b-a))
+#define const_1 ( bk/255.)
+#define const_2 (255.0/(wh-bk))
 
 float4 main(float2 tex: TEXCOORD0): COLOR {
     float4 c0 = tex2D(s0, tex);
 
-	return (c0 -const_1) *const_2;
+    return (c0 -const_1) *const_2;
 }
