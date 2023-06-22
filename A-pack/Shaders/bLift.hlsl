ab016eddf0b5def0a9114f400dc7f5834747ad0e
butterw <butterw@gmail.com>
2023-06-22 17:49:09 +0200

    Update bLift.hlsl

--- A-pack/Shaders/bLift.hlsl
+++ A-pack/Shaders/bLift.hlsl
@@ -2,14 +2,17 @@
 #define Lift 0.05
 
 /* --- bLift (dx9) --- */
-/* v1.0 (2023-06) released by butterw under GPLv3 
+/* v1.01 (2023-06) released by butterw under GPLv3 
 
-pixel.rgb = pixel.rgb*(1 -0.5*Lift) +0.5*Lift
-- rgb Lift Parameter (default value: 0.05)
-negative: darkens the image, 0: no change, positive: lightens the image
+c0 = c0*(1 -0.5*Lift) +0.5*Lift = c0 +0.5*Lift*(1-c0) 
+	with c0: pixel.rgb in [0, 1.0]
+
+- Parameter rgb Lift, 0: no change 
+negative: darkens the image 
+positive: lightens the image, ex: 0.05
   
-Lift limits image burn-out vs a brightness adjustment (rgb shift): 
-pixel.rgb = pixel.rgb + Brightness
+Lift limits image crushing
+vs. a brightness adjustment (rgb shift): c0 = c0 + Brightness
 */
 sampler s0: register(s0);
 
