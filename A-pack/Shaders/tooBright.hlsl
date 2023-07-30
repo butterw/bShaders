4dd038effd224ef8efc9c8651d05f28f7eac5bce
butterw <butterw@gmail.com>
2023-07-30 11:54:25 +0200

    v1.40

--- A-pack/Shaders/tooBright.hlsl
+++ A-pack/Shaders/tooBright.hlsl
@@ -1,30 +1,34 @@
 // $MinimumShaderProfile: ps_2_0
-#define Multiply 0.30
+#define Multiply 0.20
 
 /* --- tooBright (dx9) --- */
-
-/* v1.31 (2023-07) released by butterw under GPLv3
+/* v1.40 (2023-07) released by butterw under GPLv3
  (1 texture, 3 arithmetic)
 
-Apply when the picture is too bright. 
-Black and White point are not changed (no clipping). 
+Apply when the picture is too bright.
+Decreases brightness without clipping (Black point and White point are not changed).
 
-Photoshop Multiply Blend, brightness/contrast curve: 
-c0 = (1-Multiply)*c0 + Multiply*c0*c0
+Photoshop Multiply Blend, brightness/contrast curve:
+out = (1-Multiply)*x + Multiply*x*x = Multiply*x^2 -Multiply*x +x
 	with c0: pixel.rgb in [0, 1.0]
+	and Multiply>=0
 
-parameter Multiply [-1, 1.0], 0: no effect, ex: 0.3.
-positive: (Multiply Blend) decreases brightness and increases contrast in highlights.
-	Contrast increases linearly with input level and is maximum at the white point.
-negative: (Screen Blend)   increases brightness and constrast in shadows. 	 
+parameter Multiply [-1, 1.0], 0: no effect.
+>> Positive: (Multiply Blend) decreases brightness. Increases contrast in highlights. ex: 0.20
+	- Brightness decrease is maximum at the mid-point (-Multiply/4).
+	- Contrast = 2*Multiply*x +1-Multiply
+	Contrast increases linearly with input level and is maximum at the White point (1+Multiply).
+	Contrast: 1 at the mid-point.
+
+negative: (Screen Blend) increases brightness.
+
 */
-
 sampler s0: register(s0);
 
 
 float4 main(float2 tex: TEXCOORD0): COLOR {
 	float4 c0 = tex2D(s0, tex);
-	
+
 	c0.rgb = lerp(c0, c0*c0, Multiply).rgb;
 	return c0;
 }
