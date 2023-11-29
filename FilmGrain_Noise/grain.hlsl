2ba8469c750f70ab24cdbf2092501913b5d1b4b6
butterw <butterw@gmail.com>
2023-11-29 15:36:00 +0100

    +grain.hlsl (dx9)
    This filmgrain shader allows to apply less grain to dark and bright areas vs midtones.

--- FilmGrain_Noise/grain.hlsl
+++ FilmGrain_Noise/grain.hlsl
@@ -3,7 +3,7 @@
 #define Curve01  0.25
 #define Show_Grain 0
 
-/* --- grain (dx9) --- */ 
+/* --- grain (dx9) --- */
 /* v1.0 (2023-11) released by butterw under GPLv3
 Gaussian noise generation is from "semi-random grayscale noise.hlsl", (C) 2011 Jan-Willem Krans (janwillem32 <at> hotmail.com), license: GPLv2.
 (1 texture, 42 arithmetic) >> (1 texture, 38 arithmetic with Curve01:1).
@@ -13,18 +13,18 @@ Grain shaders typically generate a dynamic luma noise pattern and blend it with 
 This shader allows to apply less grain to dark and bright areas vs midtones by multiplying grain with a shaping function based on luma. The shaping function is a tunable parabola centered at midgray. The values in x:0 and x:1.0 are set by parameter Curve01.
 
 out = c0.rgb + noiseStrength*grain
-	with c0 and out pixel.rgb values in [0, 1.0].
-	and noiseStrength = 0.01*Strength.
+    with c0 and out pixel.rgb values in [0, 1.0].
+    and noiseStrength = 0.01*Strength.
 
 grain = gShape(luma, Curve01) *grain
-	with grain, positive or negative grayscale value in [-0.125, 0.125], random variable with gaussian distribution.
-	and gShape in [0, 1.0].
+    with grain, positive or negative grayscale value in [-0.125, 0.125], random variable with gaussian distribution.
+    and gShape in [0, 1.0].
 
 ## Parameters ##
 - Strength, percentage, ex: 16. [0 to 35.0], 0: no effect.
 - Curve01, curve shaping parameter, ex:0.25, typ [0 to 1.0], 1.0: uniform grain , 0: parabola(luma).
-gshape(luma:0) = gshape(luma:1.0) = Curve 
-- Show_Grain, 0: default, [0 or 1] 1: display only the noise pattern. 
+gshape(luma:0) = gshape(luma:1.0) = Curve
+- Show_Grain, 0: default, [0 or 1] 1: display only the noise pattern.
 
 Grain should only be added after any sharpening operations are performed (sharpening generated grain is undesirable).
 When upscaling with no post-resize sharpening, grain can be added pre-resize with some advantages:
@@ -41,19 +41,19 @@ const static float4 RandomFactors = { pow(PI, 4), exp(5), pow(13, 0.5*PI), sqrt(
 sampler s0: register(s0);
 float4  p0: register(c0);
 #define CoefLuma float3(0.2126, 0.7152, 0.0722) //sRGB, HDTV
-#define gshape(x) lerp(1 - pow((2*x -1), 2), 1, Curve01) // C + (1-C)*(1 -(2*x-1)^2) = 1 + (C-1)*(2*x-1)^2  //for negative C, use: max(gshape(luma), 0)  
+#define gshape(x) lerp(1 - pow((2*x -1), 2), 1, Curve01) // C + (1-C)*(1 -(2*x-1)^2) = 1 + (C-1)*(2*x-1)^2  //for negative C, use: max(gshape(luma), 0)
 
 float4 main(float2 tex: TEXCOORD0): COLOR {
-	float4 r_in = float4( tex.xy, p0.zw *1.0/65536 +exp(5) ); // xy input coordinates and time factors (clock and counter)
-	for(int i=0; i< n; i++) { rnd(x) rnd(y) rnd(z) rnd(w) };  // randomize
-	float grain;	
-	#if Show_Grain == 1
-		grain = 0.25 -dot(r_in, 0.125); //in [-0.125, 0.125]
-		return 0.5 + grain;
-	#endif
-	grain = 0.25*noiseStrength -dot(r_in, 0.125*noiseStrength); // noiseStrength*grain
-	float4 c0 = tex2D(s0, tex);
-	float luma = dot(c0.rgb, CoefLuma);
-	grain = gshape(luma)*grain; 
-	return c0 + grain;
+    float4 r_in = float4( tex.xy, p0.zw *1.0/65536 +exp(5) ); // xy input coordinates and time factors (clock and counter)
+    for(int i=0; i< n; i++) { rnd(x) rnd(y) rnd(z) rnd(w) };  // randomize
+    float grain;
+    #if Show_Grain == 1
+        grain = 0.25 -dot(r_in, 0.125); //in [-0.125, 0.125]
+        return 0.5 + grain;
+    #endif
+    grain = 0.25*noiseStrength -dot(r_in, 0.125*noiseStrength); // noiseStrength*grain
+    float4 c0 = tex2D(s0, tex);
+    float luma = dot(c0.rgb, CoefLuma);
+    grain = gshape(luma)*grain;
+    return c0 + grain;
 }
