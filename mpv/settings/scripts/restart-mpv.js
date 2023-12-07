88f33f21e8c6a60c913315602ca2ba58e53fa940
butterw <butterw@gmail.com>
2023-12-07 17:26:36 +0100

    Update restart-mpv.js
    v0.20 requires mpv v0.37-dev (v0.37.0-70-g562450f5)

--- mpv/settings/scripts/restart-mpv.js
+++ mpv/settings/scripts/restart-mpv.js
@@ -1,17 +1,17 @@
 'use strict';
-/* --- restart-mpv.js (mpv script) --- */
-/* v0.10 by butterw (2023/12/06), tested on Win10 with mpv 0.36 and 0.37-dev.
+mp.add_key_binding("Shift+F5", "restart-mpv", restart_mpv); // input.conf: Shift+F5 script-binding restart-mpv
+//mp.register_script_message("restart-mpv", restart_mpv);   // input.conf: Shift+F5 script-message restart-mpv
 
-keeps current shaders (and recompiles them if the source code was modified).
+/* --- restart-mpv.js (mpv script) --- */
+/* v0.20 by butterw (2023/12). tested on Win10.  
+v0.20 requires mpv v0.37-dev (v0.37.0-70-g562450f5)
+v0.11 by butterw (2023/12/06), tested on Win10 with mpv 0.36 and 0.37-dev.
+
+restarts mpv: keeps current shaders (and recompiles them if the source code was modified).
 */
 
-mp.add_key_binding("Shift+F5", "restart-mpv", restart_mpv); // (input.conf): Shift+F5 script-binding restart-mpv
-//mp.register_script_message("restart-mpv", restart_mpv);   // input.conf:   Shift+F5 script-message restart-mpv
-
 function restart_mpv() {
-	var sep = mp.get_property("platform")=="windows" ? ";": ":";
-	var s = "--glsl-shaders="+ mp.get_property_native("glsl-shaders");
-	s = s.replace(/,/g, sep);
+	var s = "--glsl-shaders="+ mp.get_property("glsl-shaders"); 
 	print("restart mpv", s);
 	mp.commandv("run", "mpv.com", mp.get_property("path"), s);
 	mp.command("quit");
