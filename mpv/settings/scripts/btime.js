75e35531b0c38c31dd3af94a84bd12663b972cb2
butterw <butterw@gmail.com>
2024-01-14 16:44:16 +0100

    btime.js v0.30 +script-opts
    <youtube, btime, symetrical, mpc-hc, mpv>
    format=btime

--- mpv/settings/scripts/btime.js
+++ mpv/settings/scripts/btime.js
@@ -1,70 +1,90 @@
 'use strict';
 
-/* --- btime.js mpv script (shortened time OSD) --- */
-/* v0.20 by butterw (2023/11) lightweight script.
+var opts = {
+    format: "btime", // <"youtube", "btime", "symetrical", "mpc-hc", "mpv">
+    seek: true,
+    ms: 1500,
+    alt: false,
+};
+
+/* --- btime.js mpv script (short time OSD) --- */
+/* v0.30 (release) by butterw (2024/01)
+
+v0.30: --script-opts-add=btime-format=mpc-hc,btime-alt=yes
+or script-opts/btime.conf
+or runtime: cycle-value script-opts btime-format=mpc-hc,btime-alt=yes
+
 v0.20: handle case where duration is undefined.
 use --script=path.to/btime.js OR install by copying to portable_config/scripts (tested on win10).
 
 Short time format for On-Screen-Display.
 ex OSD: 0:08 / 1:12, instead of 00:00:08 / 00:01:12.
-the modified time strings are shown for a limited duration on OSD on seeks and/or on demand (default show-time hotkey: F11).
-- ! mp.register_event("seek" redefines the `seek` OSD display:
-1) use with --no-osd-on-seek
-2) make sure there are no collisions with other loaded user scripts.
+the modified time strings are shown for a limited duration on OSD on seeks and/or on demand.
+
+Install
+1) define hotkey for on demand show, ex: F11
+input.conf: F11 script-binding btime/show
+2) ! with option seek=true, redefines the `seek` OSD display.
+a) use with --osd-on-seek=bar (mpv default) or --no-osd-on-seek
+b) Make sure there are no collisions with other loaded user scripts.
+3) Configurable via script-opts.
 
 The OSD time format can be customized by the user.
-by default, btime.js uses a short time format similar to the one used by youtube, but better suited for movies.
+by default, uses a short time format similar to youtube, but better suited for movies (btime).
 
-playback-time / duration
-00:00:08 / 00:01:12	 (mpv default)
-   00:08 / 01:12 (mpc-hc)
-	0:08 / 1:12	 (btime)
+playtime / duration
+00:00:08 / 00:01:12 (mpv default)
+   00:08 / 01:12    (mpc-hc, symetrical)
+    0:08 / 1:12     (btime, youtube)
+  - 1.04 / 1:12     (alt + btime)
 
-00:06:04 / 01:27:09 (mpv default)
-00:06:04 / 01:27:09 (mpc-hc)
-   06:04 / 1:27:09 (btime)
+ 00:06:04 / 01:27:09 (mpv default, mpc-hc)
+  0:06:04 / 1:27:09  (symetrical)
+    06:04 / 1:27:09  (btime)
+     6:04 / 1:27:09  (youtube)
+- 1:21:05 / 1:27:09  (alt + btime)
 
-01:02:08 / 01:27:09 (mpv default)
-01:02:08 / 01:27:09 (mpc-hc)
- 1:02:08 / 1:27:09 (btime)
+01:02:08 / 01:27:09 (mpv default, mpc-hc)
+ 1:02:08 / 1:27:09  (btime, youtube, symetrical)
+ - 25:01 / 1:27:09  (alt + btime)
 
 */
-var duration;
+var duration; //str
+mp.options.read_options(opts, "btime", on_update);
+function on_update() { //runtime update of script-opts: format, alt, ms
+    print("time format:", opts.format);
+    duration = fmt_time_str(mp.get_property_osd("duration"));
+};
 
-function fmt_time_str(t_str)  {
-	// shortens a time string, ex: duration
-	if (t_str === undefined) return "undefined";
-	if (t_str.substring(0, 3)== "00:") t_str=t_str.substring(3); //changes 00:01:12 to 01:12
-	//drop the leading zero, changes 01:12 to 1:12 and 01:27:09 to 1:27:09 :
-	if (t_str.charAt(0)=="0") t_str = t_str.substring(1); //mpc-hc format: comment out this line.
-	return t_str;
+function fmt_time_str(t_str) {
+    // shortens a time string, ex: duration
+    if (t_str === undefined) return "undefined"; //show: returned length needs to be 8 or more.
+    if (opts.format!="mpv") {
+        if (opts.format=="youtube" && t_str.substring(0, 4)== "00:0") t_str=t_str.substring(4); //youtube (playtime & duration): changes 00:01:12 to 1:12
+        else {
+            if (t_str.substring(0, 3)== "00:") t_str=t_str.substring(3); //btime, symetrical: changes 00:01:12 to 01:12
+            if (opts.format!="mpc-hc" && t_str.charAt(0)=="0") t_str = t_str.substring(1); //btime, symetrical, youtube: drop the leading zero, changes 01:12 to 1:12 and 01:27:09 to 1:27:09
+        }
+    }
+    return t_str;
 }
 
-mp.register_event("file-loaded", function() {
-	// every time a new file is loaded, process the duration string
-	duration = fmt_time_str(mp.get_property_osd("duration"));
+mp.register_event("file-loaded", function() { // every time a new file is loaded, process the duration string
+    duration = fmt_time_str(mp.get_property_osd("duration"));
 });
 
-mp.add_key_binding("F11", "show-time", on_seek);
-// input.conf: F11 script-binding show-time
-mp.register_event("seek", on_seek);
-function on_seek() {
-	var playtime = mp.get_property_osd("playback-time");
-	playtime = playtime.substring(8-duration.length); //keep the same digits as duration
-	if (duration.length==7 && playtime.charAt(0)=="0") {
-		playtime = playtime.substring(2); //drop leading zero hours, ex: 06:04 / 1:27:09
-	}
-	mp.commandv("show-text", playtime +" / "+ duration); // custom OSD
+mp.add_key_binding(null, "show", show);// input.conf: F11 script-binding btime/show
+if (opts.seek) mp.register_event("seek", show);
+function show() {
+    var playtime = opts.alt ? mp.get_property_osd("time-remaining"): mp.get_property_osd("playback-time");
+    if (playtime === undefined || (duration == "undefined" && playtime=="00:00:00")) return; //ex: image
+    if (opts.format!="mpv") {
+        if (opts.format=="youtube") playtime = fmt_time_str(playtime);
+        else {
+            playtime = playtime.substring(8-duration.length); //mpc-hc, symetrical, btime: keep the same digits as duration
+            if (opts.format=="btime" && (duration.length==7 && playtime.charAt(0)=="0")) playtime = playtime.substring(2); //btime (playtime): drop leading zero on hours, 00:06:04 to 06:04 / 1:27:09
+        }
+    }
+    playtime = "".concat(opts.alt ? "- ": "", playtime, " / ", duration);
+    mp.commandv("show-text", playtime, opts.ms); // display on OSD (message duration: ms)
 }
-
-/* This code bloc is commented out.
-function on_seek() {
-	var playtime = mp.get_property_osd("playback-time"); //"time-remaining"
-	//playtime = fmt_time_str(playtime);
-
-	// Alternative seek function that keeps the same digits as duration
-	// can be used to achieve mpc-hc mode:
-	playtime = playtime.substring(8-duration.length);
-	mp.commandv("show-text", playtime +" / "+ duration);
-}
-*/
