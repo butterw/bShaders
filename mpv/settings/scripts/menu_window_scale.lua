c76237f9e144115893fbde51b9bff90f0916264b
butterw <butterw@gmail.com>
2024-01-18 11:44:32 +0100

    . menu_window_scale.lua

--- mpv/settings/scripts/menu_window_scale.lua
+++ mpv/settings/scripts/menu_window_scale.lua
@@ -1,7 +1,8 @@
-local scales = {0.5, 0.75, 1, 1.25, 1.5, 2}
+local scales = {0.5, 0.75, 1, 1.25, 1.5, 2} -- choose menu values
+local keyword = 'window-scale'
 
 -- menu_window_scale.lua mpv script,
--- by butterw (2024-01-17)
+-- by butterw (2024-01-18)
 --
 -- Simple custom dynamic menu example using mpv-menu-plugin v2.1 with dyn_menu.lua, req mpv0.37 on windows.
 -- Adds a Scale menu populated with window-scale values (configured in scales variable), this script ensures the current value is checked.
@@ -14,7 +15,7 @@ local function printd(pre, x) print(pre, utils.to_string(x)) end --printd("x:", 
 -- initial menu setup
 local submenu={}
 for _, elt in ipairs(scales) do
-    local pre = elt==1 and '=' or '' --adds '=' menu accelerator for the value 1
+    local pre = elt==1 and '=' or '' --adds '=' menu accelerator for the value 1.0
     submenu[#submenu + 1] = {
         title = pre..elt ..'x',
         cmd = string.format('set fullscreen no; set window-scale %s', elt), -- the command that will be applied. 
@@ -22,10 +23,10 @@ for _, elt in ipairs(scales) do
     }
 end
 
--- wait until the menu is ready, then get menu info for keyword 'window-scale'
+-- wait until the menu is ready, then get menu info for desired keywords 
 mp.register_script_message('menu-ready', function()
     -- print('menu-ready');
-    mp.commandv('script-message-to', 'dyn_menu', 'get', 'window-scale', mp.get_script_name())
+    mp.commandv('script-message-to', 'dyn_menu', 'get', keyword, mp.get_script_name())
 end)
 
 -- if keyword is present (in input.conf), observe property window-scale 
@@ -33,13 +34,13 @@ mp.register_script_message('menu-get-reply', function(data)
     local reply = utils.parse_json(data)
     -- printd("reply:", reply)
     -- printd("error:", reply.error)
-    if not reply.error and reply.keyword == 'window-scale' then mp.observe_property('window-scale', 'native', window_scale_checklist_cb) end
+    if not reply.error and reply.keyword == keyword then mp.observe_property(keyword, 'native', window_scale_checklist_cb) end
 end)
 
 -- update checked state of submenu entries based on window-scale value
 function window_scale_checklist_cb(_, value)
     -- print(_, value)
-    if not value then return end
+    if value == nil then return end
 
     for i, elt in ipairs(scales) do
         local state = elt==value and {'checked'} or {}
@@ -48,5 +49,5 @@ function window_scale_checklist_cb(_, value)
     local item = {type='submenu'}
     item.submenu = submenu
     -- printd("item:", item)
-    mp.commandv('script-message-to', 'dyn_menu', 'update', 'window-scale', utils.format_json(item))
+    mp.commandv('script-message-to', 'dyn_menu', 'update', keyword, utils.format_json(item))
 end
